c3203e41bf3bc1380dc876036d6ab315636477d0
OscarLpz95 <oscarr_lopez@hotmail.com>
2015-02-26 11:36:39 -0600

    Create WSQ07.py

---
+++ WSQ07.py
@@ -0,0 +1,9 @@
+print("Oscar Ricardo López López")
+x = int(input("Please give us the lower bound: "))
+y = int(input("Please give us the upper bound: "))
+sum = x
+c = x
+while(c<y):
+    c = c+1
+    sum = sum+c
+print("The sum from",x ," to ",y ," (inclusive) is: ",sum)
