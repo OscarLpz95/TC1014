7c21c42f9fdf0438cffddb091c2c4c54ad18a52c
OscarLpz95 <oscarr_lopez@hotmail.com>
2015-02-26 11:34:45 -0600

    Create WSQ03.py

---
+++ WSQ03.py
@@ -0,0 +1,15 @@
+print("Fun with Numbers, by Oscar Lopez")
+x= 0
+y= 0
+x = (int(input("Input a number")))
+y = (int(input("Input another number")))
+S = x + y
+print("The sum of the two numbers is = ", S)
+D = x - y
+print("The sum of the two numbers is = ", D)
+P = x * y
+print("The product of two numbers is = ", P)
+I = x // y
+print("The integer based division of the two numbers is = ", I)
+Z = x % y
+print("The remainder of integer division of the two numbers is = ", Z)
